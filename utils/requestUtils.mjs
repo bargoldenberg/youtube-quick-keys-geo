ab4422b88034e4439a070cf5b8401017b20cdd68
Bar Goldenberg <bar.goldenberg@similarweb.com>
2025-03-22 16:05:14 +0200

    extract ip

--- utils/requestUtils.mjs
+++ utils/requestUtils.mjs
@@ -1,11 +1,3 @@
 export const extractIPFromRequest = (req) => {
-  console.log(req.ip);
-  console.log(req.headers["x-real-ip"]);
-  console.log(req.headers["x-forwarded-for"]);
-
-  const forwarded = req.headers["x-forwarded-for"];
-  const ip = forwarded
-    ? forwarded.split(",").pop().trim()
-    : req.connection.remoteAddress;
-  return ip;
+  return req.ip;
 };
