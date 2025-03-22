dc0f1cbc142b272bf4ab975288438f240dba9097
Bar Goldenberg <bar.goldenberg@similarweb.com>
2025-03-22 23:43:34 +0200

    refactor

--- controllers/geo.controller.js
+++ controllers/geo.controller.js
@@ -1,12 +1,13 @@
 import { extractIPFromRequest } from "../utils/requestUtils.mjs";
 import { getGeoData } from "../services/geo.service.js";
+
 export const geo = async (req, res) => {
   try {
     const ip = extractIPFromRequest(req);
     const geoData = await getGeoData(ip);
     await res.json(geoData);
   } catch (e) {
-    console.log(e);
+    console.error(e);
     res.sendStatus(500);
   }
 };
